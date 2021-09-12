d1bb96d1fc746598237f476f9d0fa0b8dc819df3
HighPon <mjoppaida@icloud.com>
2021-09-13 03:34:54 +0900

    initinal heroku deploy

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"hackaichi2021/auth"
+
+	"github.com/gorilla/mux"
+)
+
+type post struct {
+	Title string `json:"title"`
+	Tag   string `json:"tag"`
+	URL   string `json:"url"`
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.Handle("/public", public)
+	r.Handle("/private", auth.JwtMiddleware.Handler(private))
+	r.Handle("/auth", auth.GetTokenHandler)
+
+	//サーバー起動
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), r); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
+
+var public = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	post := &post{
+		Title: "test",
+		Tag:   "test",
+		URL:   "test",
+	}
+	json.NewEncoder(w).Encode(post)
+})
+
+var private = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	post := &post{
+		Title: "test",
+		Tag:   "test",
+		URL:   "test",
+	}
+	json.NewEncoder(w).Encode(post)
+})
