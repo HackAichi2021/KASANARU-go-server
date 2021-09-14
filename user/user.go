37410fe7fe092d0338abc18fd7f3b03107919939
HighPon <mjoppaida@icloud.com>
2021-09-14 14:45:16 +0900

    delete waste code

--- user/user.go
+++ user/user.go
@@ -2,7 +2,6 @@ package user
 
 import (
 	"encoding/json"
-	"fmt"
 	"hackaichi2021/database"
 	"net/http"
 )
@@ -10,7 +9,6 @@ import (
 var Register = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	var form database.User
 	json.NewDecoder(r.Body).Decode(&form)
-	fmt.Println("call api")
 	database.CreateUser(form)
 	json.NewEncoder(w).Encode(form)
 
